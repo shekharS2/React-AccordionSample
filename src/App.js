3f7438a2d510f5f483f3888f3efdd227d0d0e56f
shekharS2 <saikiashekhar25@gmail.com>
2022-07-12 17:05:16 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import data from './data.json';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [selected, setSelected] = useState(null);
+
+	const toggle = (i) => {
+		if(selected === i) {
+			return setSelected(null);
+		}
+
+		setSelected(i);
+	}
+
+    return (
+        <div className='app-container'>
+			<div className='accordian'>
+				{
+					data.map((item, i) => {
+						return <div className='item'>
+							<div className='question' onClick={() => toggle(i)}>
+								<h2>{item.question}</h2>
+								<span>{selected === i ? '-' : '+'}</span>
+							</div>
+							<div 
+								className={
+									selected === i ? 'answer show' : 'answer'
+								}
+							>
+								{item.answer}
+							</div>
+						</div>
+					})
+				}
+			</div>
+        </div>
+    );
 }
 
 export default App;
